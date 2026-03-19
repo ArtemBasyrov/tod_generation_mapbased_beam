9a2a931cfbfaa40eda8d2524d4d17f333d257d5d
ArtemBasyrov <artem.m.basyrov@gmail.com>
2026-03-19 20:22:12 +0100

    mask

--- sample_based_tod_generation_config.py
+++ sample_based_tod_generation_config.py
@@ -1,12 +1,12 @@
-FOLDER_BEAM = '/Users/basyrov/Documents/APC/furax_mapbased_beam/'
-FOLDER_SCAN = '/Users/basyrov/Documents/APC/tod_from_beam_generation/scan_strategy/'
-FOLDER_TOD_OUTPUT = '/Users/basyrov/Documents/APC/tod_from_beam_generation/tod_output_nside1024_30arcmin_res1arcmin_gridint_test/'
+FOLDER_BEAM = '/path/to/beam/folder/'
+FOLDER_SCAN = '/path/to/scan/folder/'
+FOLDER_TOD_OUTPUT = '/path/to/tod/output/folder/'
 
-path_to_map = '/Users/basyrov/Documents/APC/generated_data/cmb_only/sky_map_freq_90_1024_raw.fits'
+path_to_map = '/path/to/map/skymap.fits'
 
-beam_file_I = 'spherical_beam_pixel_map_30arcmin_res_1arcmin.fits'
-beam_file_Q = 'spherical_beam_pixel_map_30arcmin_res_1arcmin.fits'
-beam_file_U = 'spherical_beam_pixel_map_30arcmin_res_1arcmin.fits'
+beam_file_I = 'beam_pixel_map.fits'
+beam_file_Q = 'beam_pixel_map.fits'
+beam_file_U = 'beam_pixel_map.fits'
 
 start_day = 0
 end_day = 366
